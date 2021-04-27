53e645273248bf9f915f73242c7093ee4b48baf5
ivan_gorchichko <ivangorchichko@mail.ru>
2021-04-28 00:37:45 +0300

    Layout of main page was created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
+import Button from './components/buttons/Button';
+import Input from './components/input/Input';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: "0",
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="calc">
+          <div className="row">
+            <Input input={this.state.input} />
+          </div>
+          <div className="bg-digits">
+            <div className="row">
+              <Button value="C" />
+              <Button value="+/-" />
+              <Button value="%" />
+              <Button value="&divide;" />
+            </div>
+            <div className="row">
+              <Button value="7" />
+              <Button value="8" />
+              <Button value="9" />
+              <Button value="&times;" />
+            </div>
+            <div className="row">
+              <Button value="4" />
+              <Button value="5" />
+              <Button value="6" />
+              <Button value="-" />
+            </div>
+            <div className="row">
+              <Button value="1" />
+              <Button value="2" />
+              <Button value="3" />
+              <Button value="+" />
+            </div>
+            <div className="row">
+              <Button value="0" />
+              <Button value="." />
+              <Button value="=" />
+            </div>
+          </div>
+        </div>
+      </div >
+    );
+  }
 }
 
 export default App;
